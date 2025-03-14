14496c3adee232ba757d3e50f1c0111a57c17ff0
Silvia Alsina <134503707+salsi95@users.noreply.github.com>
2025-03-14 12:46:00 +0100

    eda preliminar

---
+++ src/sp_eda.py
@@ -0,0 +1,55 @@
+# Tratamiento de datos
+import pandas as pd
+
+
+
+def eda_preliminar (df):
+
+    """
+    Realiza un análisis exploratorio preliminar sobre un DataFrame dado.
+
+    Este análisis incluye:
+    - Muestra aleatoria de 5 filas del DataFrame.
+    - Información general del DataFrame (tipo de datos, nulos, etc.).
+    - Porcentaje de valores nulos por columna.
+    - Conteo de filas duplicadas.
+    - Distribución de valores para columnas categóricas.
+
+    Parameters:
+    df (pd.DataFrame): DataFrame a analizar.
+
+    Returns:
+    None
+    """
+
+
+    display(df.sample(5))
+
+    print('-----------------')
+
+    print('DIMENSIONES')
+
+    print(f'Nuestro conjunto de datos presenta un total de {df.shape[0]} filas y {df.shape[1]} columnas')
+
+    print('-----------------')
+
+    print('INFO')
+
+    display(df.info())  
+
+    print('-----------------')
+    print('NULOS')
+
+    display(df.isnull().mean()*100)
+
+    print('-----------------')
+    print('DUPLICADOS')
+
+    print(f'Tenemos un total de {df.duplicated().sum()} duplicados')
+
+    print('-----------------')
+    print('FRECUENCIA CATEGÓRICAS')
+    for col in df.select_dtypes(include= 'object').columns:
+        print(col.upper())
+        print(df[col].value_counts())
+        print('-------')
